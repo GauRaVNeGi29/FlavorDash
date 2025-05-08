eeb87297812ec7219e5fd0f80ba6ea3b1763e592
negi_gaurav <negigaurav419@gmail.com>
2025-05-08 19:57:42 +0530

    8/5/25

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   server:{
     proxy:{
       '/api':'http://localhost:5000',
     },
+  },
+  build: {
+    outDir: 'dist',
   },
   plugins: [react()],
   resolve: {
